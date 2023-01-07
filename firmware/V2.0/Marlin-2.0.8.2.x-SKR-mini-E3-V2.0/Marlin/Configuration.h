5222e8efc45d128fea6694b257ee6071b9ce4ed4
Lefix <lefix@posteo.de>
2023-01-07 13:15:24 +0100

    [minor] adjusting bed size

--- firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.8.2.x-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -1242,15 +1242,15 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 235
+#define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -45
+#define Y_MIN_POS -20
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 190
+#define Y_MAX_POS 215
 #define Z_MAX_POS 250
 
 /**
